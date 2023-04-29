df35d161066f0ed31a672ad44955ded6a87c4e19
Jacintamayol94 <jacimayol94@gmail.com>
2023-04-29 12:43:42 -0300

    formulariocss

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const app = express ();
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
-app.listen (3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
 
+app.get('/formlogin', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/formlogin.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+
+app.listen (3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
